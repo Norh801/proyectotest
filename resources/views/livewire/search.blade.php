e06ad113efeb3ac4dc68228552a3687ad4aab22c
Norh801 <oskarcg2001@gmail.com>
2022-04-25 16:57:53 -0500

    la ultima pls 10

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -2,7 +2,7 @@
 <div>
 <div class="col-sm-12 col-md-12">
     <div class="form-group">
-        <select id="code" class="form-control nested" style="width:100%!important;">
+        <select id="code" class="form-control nested" style="width:100%!important;" wire:model='code'>
             <div>
                 @foreach ($data as $product)
                     <option value="{{$product->code}}">{{$product->name}}</option>
@@ -12,8 +12,14 @@
     </div>
 </div>
 <div class="col-lg-12 col-sm-12 mb-3">
-    <button wire:click.prevent="$emit('scan-code', $('#code').val())" type="button" class="btn btn-outline-dark mb-2" id="agregar">
+    <button wire:click.prevent="addCart($('#code').val())" type="button" class="btn btn-outline-dark mb-2" id="agregar">
         Agregar (Shift a)
     </button>
 </div>
+
+<script>
+    function addCart(id){
+        window.livewire.emit('scan-code', id);
+    }
+</script>
 </div>
